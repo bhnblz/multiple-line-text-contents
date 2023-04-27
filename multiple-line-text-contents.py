d1cab94d01e90f36e38d103fdc6d3b85ce07d253
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 21:29:39 +0800

    open file and input line

--- multiple-line-text-contents.py
+++ multiple-line-text-contents.py
@@ -1,6 +1,13 @@
-# Open a file
+def write_lines():
+    # Open a file
+    with open("write_file.txt", "w") as my_file:
         # while true
+        while True:
             # Input line
+            line = input("Enter line: ")
+            my_file.write(line)
+    
             # If you will input more lines
+            
             # If choices is N, break
 # Call the function
