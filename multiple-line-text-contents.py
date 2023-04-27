4d0dfa3c2f2deeb237f202de4f48cbbcc990521e
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 22:01:38 +0800

    adding python module application

--- multiple-line-text-contents.py
+++ multiple-line-text-contents.py
@@ -1,10 +1,21 @@
+import colorama
+from colorama import Fore
+import pyfiglet
+
+greetings = "Hello!"
+print(pyfiglet.figlet_format (greetings.center(32), font = "slant"))
+description = "This is a python program that will create a multiple line of text."
+print(Fore.LIGHTMAGENTA_EX + ("*" * 80))
+print(Fore.LIGHTMAGENTA_EX + (description.center(80)))
+print("*" * 80)
+
 def write_lines():
     # Open a file
     with open("write_file.txt", "w") as my_file:
         # while true
         while True:
             # Input line
-            line = input("Enter line: ")
+            line = input(Fore.LIGHTCYAN_EX + "Enter line: ")
             my_file.write(line)
     
             # If you will input more lines
